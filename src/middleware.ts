a98a370aae922d739983385fa90847a26e1e2161
PedroAugSouza <psouza.880@icloud.com>
2024-12-13 18:17:34 -0300

    feat: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,43 @@
+import { MiddlewareConfig, NextRequest, NextResponse } from 'next/server';
+import { COOKIE_KEY } from './shared/constants';
+import { jwtDecode } from 'jwt-decode';
+import { IToken } from './shared/types/middleware.contact';
+import { RoleUser } from './shared/value-objects/role-user.value-object';
+
+export const middleware = async (request: NextRequest) => {
+  const cookie = request.cookies.get(COOKIE_KEY);
+  const token = cookie?.value;
+  // const tokenParsed = jwtDecode(token)
+  const loginUrl = new URL('/login', request.url);
+  const registerUrl = new URL('/register', request.url);
+  const homeUrl = new URL('/', request.url);
+  const adminUrl = new URL('/admin', request.url);
+
+  if (token) {
+    const tokenParsed: IToken = jwtDecode(token);
+    if (
+      request.nextUrl.pathname === '/login' ||
+      request.nextUrl.pathname === '/login'
+    ) {
+      if (tokenParsed.role === RoleUser.ADMIN) {
+        return NextResponse.redirect(adminUrl);
+      }
+      return NextResponse.redirect(homeUrl);
+    } else {
+      return NextResponse.next();
+    }
+  } else {
+    if (
+      request.nextUrl.pathname === '/' ||
+      request.nextUrl.pathname.startsWith('/admin')
+    ) {
+      return NextResponse.redirect(loginUrl);
+    } else {
+      return NextResponse.next();
+    }
+  }
+};
+
+export const config: MiddlewareConfig = {
+  matcher: ['/', '/admin', '/login', '/register'],
+};
---
+++ src/shared/types/middleware.contact.ts
@@ -0,0 +1,6 @@
+export interface IToken {
+  sub: string;
+  username: string;
+  email: string;
+  role: string;
+}
